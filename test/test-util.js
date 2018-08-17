1c4ded661fd3568788b7c50fd7a122e719e103b1
FirmaChain-Official-Developer <developer@firma-solutions.com>
2018-08-17 02:27:22 +0000

    Add utility function for test

--- test/test-util.js
+++ test/test-util.js
@@ -2,9 +2,24 @@ module.exports = {
 	wei:function(n) {
 	  return new web3.BigNumber(String(web3.toWei(n, 'ether')));
 	},
+	getBlock: function(value) {
+		return new Promise((resolve, reject) => {
+			web3.eth.getBlock(value, (e, r) => e ? reject(e) : resolve(r))
+		});
+	},
 	getBlockNumber: function() {
 		return new Promise((resolve, reject) => {
 			web3.eth.getBlockNumber((e, r) => e ? reject(e) : resolve(r))
+		});
+	},
+	sendTransaction: function(value) {
+		return new Promise((resolve, reject) => {
+			web3.eth.sendTransaction(value, (e, r) => e ? reject(e) : resolve(r))
+		});
+	},
+	getBalance: function(value) {
+		return new Promise((resolve, reject) => {
+			web3.eth.getBalance(value, (e, r) => e ? reject(e) : resolve(r))
 		});
 	},
 }
