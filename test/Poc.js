bd61c84f54f7e2f3926d6accf957fa033ed0fbec
FirmaChain-Official-Developer <developer@firma-solutions.com>
2018-09-03 08:30:50 +0000

    Categorize test for Poc

--- test/Poc.js
+++ test/Poc.js
@@ -8,7 +8,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should();
 
-contract('Poc', function(accounts) {
+contract('Proof of Concept', function(accounts) {
 	let poc;
 	let partiesOfContractBefore;
 	let partiesOfContractAfter;
@@ -29,90 +29,101 @@ contract('Poc', function(accounts) {
 		poc = await Poc.new({from: owner});
 	});
 
-	it('Create New Contract with short lifetime', async () => {
-		await poc.newContract(contractId0, parties, 1).should.be.fulfilled;
+	describe('Expiration test', () => {
+		it('Create New Contract with short lifetime', async () => {
+			await poc.newContract(contractId0, parties, 1).should.be.fulfilled;
+		});
 	});
 
-	it('Reject contract in a person', async () => {
-		await poc.newContract(contractId, [owner], 100).should.be.rejected;
+	describe('Creation condition test', () => {
+		it('Reject contract in a person', async () => {
+			await poc.newContract(contractId, [owner], 100).should.be.rejected;
+		});
+
+		it('Reject contract whose lifetime is zero', async () => {
+			await poc.newContract(contractId, [owner], 0).should.be.rejected;
+		});
+
+		it('Reject contract with duplicated parties', async () => {
+			await poc.newContract(contractId, [owner, owner], 0).should.be.rejected;
+			await poc.newContract(contractId, [parties[0], parties[1], parties[0]], 0).should.be.rejected;
+		});
 	});
 
-	it('Reject contract whose lifetime is zero', async () => {
-		await poc.newContract(contractId, [owner], 0).should.be.rejected;
+	describe('Sign & extra data test', () => {
+		it('Create New Contract among two', async () => {
+			await poc.newContract(contractId, parties, 100).should.be.fulfilled;
+		});
+
+		it('Reject duplicated signs', async () => {
+			await poc.signContract(contractId, contractSign, "extraData", {from: parties[0]}).should.be.fulfilled;
+			await poc.signContract(contractId, contractSign, "extraData", {from: parties[0]}).should.be.rejected;
+		});
+
+		it('Reject sign by person not in party', async () => {
+			await poc.signContract(contractId, contractSign, "extraData", {from: parties3[2]}).should.be.rejected;
+		});
+
+		it('Complete contract two sign', async () => {
+			let resultBefore = await poc.getStatus(contractId);
+			resultBefore.should.be.bignumber.equal(0);
+			await poc.signContract(contractId, contractSign, "extraData", {from: parties[1]}).should.be.fulfilled;
+			let resultAfter = await poc.getStatus(contractId);
+			resultAfter.should.be.bignumber.equal(1);
+		});
+
+		it('Get extra data', async () => {
+			let extraData = await poc.extraData(contractId, 1);
+			extraData.should.equal("extraData");
+		});
 	});
 
-	it('Reject contract with duplicated parties', async () => {
-		await poc.newContract(contractId, [owner, owner], 0).should.be.rejected;
-		await poc.newContract(contractId, [parties[0], parties[1], parties[0]], 0).should.be.rejected;
+	describe('Sign integration test', () => {
+		it('Fail to accept contract with different sign', async () => {
+			await poc.newContract(contractId3, parties3, 100).should.be.fulfilled;
+			partiesOfContractBefore = await poc.getParties(contractId3);
+			await poc.signContract(contractId3, contractSign, "extraData", {from: parties3[0]}).should.be.fulfilled;
+			await poc.signContract(contractId3, contractSignWrong, "extraData", {from: parties3[1]}).should.be.fulfilled;
+			await poc.signContract(contractId3, contractSign, "extraData", {from: parties3[2]}).should.be.fulfilled;
+			let result = await poc.getStatus(contractId3);
+			result.should.be.bignumber.equal(-1);
+		});
 	});
 
-	it('Create New Contract among two', async () => {
-		await poc.newContract(contractId, parties, 100).should.be.fulfilled;
+	describe('Contract overwrite test', () => {
+		it('Reject contract with duplicated approved contract id', async () => {
+			await poc.newContract(contractId, parties, 100).should.be.rejected;
+		});
+
+		it('Accept overwrited contract', async () => {
+			await poc.newContract(contractId3, parties, 100).should.be.fulfilled;
+		});
+
+		it('Accept overwrited contract whose lifetime is over', async () => {
+			await poc.newContract(contractId0, parties, 1).should.be.fulfilled;
+		});
+
+		it('Check the removal of old data', async () => {
+			let partiesOfContractAfter = await poc.getParties(contractId3);
+			JSON.stringify(partiesOfContractBefore).should.be.not.equal(JSON.stringify(partiesOfContractAfter));
+		});
 	});
 
-	it('Reject duplicated signs', async () => {
-		await poc.signContract(contractId, contractSign, "extraData", {from: parties[0]}).should.be.fulfilled;
-		await poc.signContract(contractId, contractSign, "extraData", {from: parties[0]}).should.be.rejected;
-	});
+	describe('Subcontract test', () => {
+		it('Make new contract', async () => {
+			await poc.newContract(contractIdRoot, parties, 100).should.be.fulfilled;
+			await poc.newSubContract(contractIdSub, contractIdRoot, 100).should.be.fulfilled;
+			let partiesOfContractRoot = await poc.getParties(contractIdRoot);
+			let partiesOfContractSub = await poc.getParties(contractIdSub);
+			JSON.stringify(parties).should.be.equal(JSON.stringify(partiesOfContractRoot));
+			JSON.stringify(partiesOfContractRoot).should.be.equal(JSON.stringify(partiesOfContractSub));
+		});
 
-	it('Reject sign by person not in party', async () => {
-		await poc.signContract(contractId, contractSign, "extraData", {from: parties3[2]}).should.be.rejected;
-	});
-
-	it('Complete contract two sign', async () => {
-		let resultBefore = await poc.getStatus(contractId);
-		resultBefore.should.be.bignumber.equal(0);
-		await poc.signContract(contractId, contractSign, "extraData", {from: parties[1]}).should.be.fulfilled;
-		let resultAfter = await poc.getStatus(contractId);
-		resultAfter.should.be.bignumber.equal(1);
-	});
-
-	it('Get extra data', async () => {
-		let extraData = await poc.extraData(contractId, 1);
-		extraData.should.equal("extraData");
-	});
-
-	it('Reject contract with duplicated approved contract id', async () => {
-		await poc.newContract(contractId, parties, 100).should.be.rejected;
-	});
-
-	it('Fail to accept contract with different sign', async () => {
-		await poc.newContract(contractId3, parties3, 100).should.be.fulfilled;
-		partiesOfContractBefore = await poc.getParties(contractId3);
-		await poc.signContract(contractId3, contractSign, "extraData", {from: parties3[0]}).should.be.fulfilled;
-		await poc.signContract(contractId3, contractSignWrong, "extraData", {from: parties3[1]}).should.be.fulfilled;
-		await poc.signContract(contractId3, contractSign, "extraData", {from: parties3[2]}).should.be.fulfilled;
-		let result = await poc.getStatus(contractId3);
-		result.should.be.bignumber.equal(-1);
-	});
-
-	it('Accept overwrited contract', async () => {
-		await poc.newContract(contractId3, parties, 100).should.be.fulfilled;
-	});
-
-	it('Accept overwrited contract whose lifetime is over', async () => {
-		await poc.newContract(contractId0, parties, 1).should.be.fulfilled;
-	});
-
-	it('Check the removal of old data', async () => {
-		let partiesOfContractAfter = await poc.getParties(contractId3);
-		JSON.stringify(partiesOfContractBefore).should.be.not.equal(JSON.stringify(partiesOfContractAfter));
-	});
-
-	// SubContract Test
-	it('Make new contract', async () => {
-		await poc.newContract(contractIdRoot, parties, 100).should.be.fulfilled;
-		await poc.newSubContract(contractIdSub, contractIdRoot, 100).should.be.fulfilled;
-		let partiesOfContractRoot = await poc.getParties(contractIdRoot);
-		let partiesOfContractSub = await poc.getParties(contractIdSub);
-		JSON.stringify(parties).should.be.equal(JSON.stringify(partiesOfContractRoot));
-		JSON.stringify(partiesOfContractRoot).should.be.equal(JSON.stringify(partiesOfContractSub));
-	});
-
-	it('Check contract tree', async () => {
-		let expectedHistory = [contractIdRoot, contractIdSub];
-		let actualHistory = await poc.getContractHistory(contractIdSub);
-		JSON.stringify(actualHistory).should.be.equal(JSON.stringify(expectedHistory));
+		it('Check contract tree', async () => {
+			let expectedHistory = [contractIdRoot, contractIdSub];
+			let actualHistory = await poc.getContractHistory(contractIdSub);
+			JSON.stringify(actualHistory).should.be.equal(JSON.stringify(expectedHistory));
+		});
 	});
 
 });
