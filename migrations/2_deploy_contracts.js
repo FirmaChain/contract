c69de160b9d69190f0cedb9e9cbd2f775178a7fc
firma-official-developer <developer@firma-solutions.com>
2019-10-16 05:30:12 +0900

    new migration code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 var FCT = artifacts.require('FCT');
+var FCTV = artifacts.require('FCTV');
 var TokenLock = artifacts.require('TokenLock');
 var TokenLockDistribute = artifacts.require('TokenLockDistribute');
 
 module.exports = async (deployer, network) => {
 	deployer.then( async() => {
-		await deployer.deploy(FCT);
-		await deployer.deploy(TokenLock, FCT.address)
-		await deployer.deploy(TokenLockDistribute, FCT.address, TokenLock.address)
+		// await deployer.deploy(FCT);
+		// await deployer.deploy(TokenLock, FCT.address)
+		// await deployer.deploy(TokenLockDistribute, FCT.address, TokenLock.address)
+		await deployer.deploy(FCTV);
 	})
 };
